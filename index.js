85cc81fa929a51442b8130a0789eb20e6c4c4a24
faishalsiddiqui759 <faishalsiddiqui759@gmail.com>
2024-10-31 22:35:27 +0530

    r

--- index.js
+++ index.js
@@ -1,28 +1,27 @@
 const express = require("express");
 const app = express();
+const { v4 : uuidv4 } = require("uuid");
 let port = 8080;
 const path = require("path");
 
-app.listen(port, ()=>{
-    console.log("app is listening on port", port);
-})
+
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "/views"));
-app.use(express.static(path.join(__dirname, "/public")));
+app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({extended:true}));
 
 let posts = [
-    {
+    {   id: uuidv4(),
         username: "Faishal Siddiqui",
         content: " I am pursuing b.tech degree from reputed institude 'NATIONAL INSTITUTE OF TECHNOLOGY,M SRINAGAR'"
 
     },
-    {
+    {   id: uuidv4(),
         username: "Taufik Siddiqui",
         content: " I am currently working on youtube content"
     },
-    {
+    {   id: uuidv4(),
         username: "Shafeeq Siddiqui",
         content: " Hello i there i am excited to share that i am creating youtube content."
     }
@@ -38,7 +37,17 @@ app.get("/posts/new", (req, res)=>{
 
 app.post("/posts", (req, res)=>{
   let {username, content} = req.body;
-  posts.push({username, content});
+  let id = uuidv4();
+  posts.push({username, content, id});
   res.redirect("http://localhost:8080/posts");
 });
 
+app.get("/posts/:id", (req, res)=>{
+    let { id } = req.params;
+    let post = posts.find((p)=> id === p.id);
+    res.render("show.ejs", {post});
+});
+
+app.listen(port, ()=>{
+    console.log("app is listening on port", port);
+});
